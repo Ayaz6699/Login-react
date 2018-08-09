1669a0b2fa107e53ea85d4c97789b9c626c1422d
ayaz6699 <ayaz6699@gmail.com>
2018-08-09 19:04:06 +1000

    login-react-form

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './Form.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          
+          <h2>Login Page</h2>
+        </div>
+        <Form />
+        <h4>
+        </h4>
+      </div>
+    );
+  }
+}
+
+export default App;
